2dbf3d940324a7fa205f55e2f45d96ba573af0bd
ekhiw <ekkyhiw@gmail.com>
2022-04-11 17:35:35 +0700

    [Ekhiw] delete unittest

--- app/src/androidTest/java/com/jem/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jem/ExampleInstrumentedTest.kt
@@ -1,24 +1,24 @@
 package com.jem
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
+//import androidx.test.platform.app.InstrumentationRegistry
+//import androidx.test.ext.junit.runners.AndroidJUnit4
+//
+//import org.junit.Test
+//import org.junit.runner.RunWith
+//
+//import org.junit.Assert.*
+//
+///**
+// * Instrumented test, which will execute on an Android device.
+// *
+// * See [testing documentation](http://d.android.com/tools/testing).
+// */
+//@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
+//    @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.jem", appContext.packageName)
+//        val appContext = InstrumentationRegistry.getTargetContext()
+//        assertEquals("com.jem", appContext.packageName)
     }
 }
